128d48da5be1bceb214ed1983fe07779f64b5c35
Thi <114497724+ThiYum@users.noreply.github.com>
2023-12-01 09:28:43 -0300

    sons

--- main.js
+++ main.js
@@ -1,2 +1,55 @@
 alert("Olá mundo!");
 
+function tocaSomRisada (){
+    document.querySelector('#som_tecla_risada').play();
+}
+
+document.querySelector('.tecla tecla_risada').onclick = tocaSomRisada;
+
+function tocaSomVaia (){
+    document.querySelector('#som_tecla_vaia').play();
+}
+
+document.querySelector('.tecla tecla_vaia').onclick = tocaSomVaia;
+
+function tocaSomAplausos (){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+
+document.querySelector('.tecla tecla_aplausos').onclick = tocaSomAplausos;
+
+function tocaSomErrado (){
+    document.querySelector('#som_tecla_errado').play();
+}
+
+document.querySelector('.tecla tecla_errado').onclick = tocaSomErrado;
+
+function tocaSomGrilo (){
+    document.querySelector('#som_tecla_grilo').play();
+}
+
+document.querySelector('.tecla tecla_grilo').onclick = tocaSomGrilo;
+
+function tocaSomTambores (){
+    document.querySelector('#som_tecla_tambores').play();
+}
+
+document.querySelector('.tecla tecla_tambores').onclick = tocaSomTambores;
+
+function tocaSomTrombeta (){
+    document.querySelector('#som_tecla_trombeta').play();
+}
+
+document.querySelector('.tecla tecla_trombeta').onclick = tocaSomTrombeta;
+
+function tocaSomDinheiro (){
+    document.querySelector('#som_tecla_dinheiro').play();
+}
+
+document.querySelector('.tecla tecla_dinheiro').onclick = tocaSomDinheiro;
+
+function tocaSomVitoria (){
+    document.querySelector('#som_tecla_vitoria ').play();
+}
+
+document.querySelector('.tecla tecla_vitoria').onclick = tocaSomVitoria;
